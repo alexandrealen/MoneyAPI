70b8ea07cbad804ed6530562107f5a8a208da2a6
raviston <alexandrealen@hotmail.com>
2021-04-03 20:17:36 -0300

    money api

---
+++ MoneyAPI/MoneyAPI/Properties/CountryProperties.cs
@@ -0,0 +1,28 @@
+﻿
+namespace MoneyAPI.Properties
+{
+    public class CountryProperties
+    {
+        public string alpha3 { get; private set; }
+        public string currencyId { get; private set; }
+        public string currencyName { get; private set; }
+        public string currencySymbol { get; private set; }
+        public string id { get; private set; }
+        public string name { get; private set; }
+
+        public CountryProperties(string alpha3 = null,
+                                 string currencyId = null, 
+                                 string currencyName = null, 
+                                 string currencySymbol = null,
+                                 string id = null,
+                                 string name = null)
+        {
+            this.alpha3 = alpha3;
+            this.currencyId = currencyId;
+            this.currencyName = currencyName;
+            this.currencySymbol = currencySymbol;
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ MoneyAPI/MoneyAPI/Properties/CurrencyProperties.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MoneyAPI.Properties
+{
+    public class CurrencyProperties
+    {
+        public string currencyName { get; private set; }
+        public string id { get; private set; }
+        public string currencySymbol { get; private set; }
+
+        public CurrencyProperties(string currencyName = null, 
+                                  string id = null, 
+                                  string currencySymbol = null)
+        {
+            this.id = id;
+            this.currencyName = currencyName;
+            this.currencySymbol = currencySymbol;
+        }
+    }
+}
